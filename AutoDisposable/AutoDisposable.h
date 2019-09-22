f6a55020f236fc00d091da891c6073703ab336d4
Vladislav Ternovskiy <vladislav.ternovskii@gmail.com>
2019-09-22 12:06:55 +0300

    Initial Commit

---
+++ AutoDisposable/AutoDisposable.h
@@ -0,0 +1,19 @@
+//
+//  AutoDisposable.h
+//  AutoDisposable
+//
+//  Created by Владислав Терновский on 9/22/19.
+//  Copyright © 2019 Vladyslav Ternovskyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoDisposable.
+FOUNDATION_EXPORT double AutoDisposableVersionNumber;
+
+//! Project version string for AutoDisposable.
+FOUNDATION_EXPORT const unsigned char AutoDisposableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoDisposable/PublicHeader.h>
+
+
