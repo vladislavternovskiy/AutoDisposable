4d29b1b1aa4fad533aa6f6d62f56601f82279b92
Vladislav Ternovskiy <vladislav.ternovskii@gmail.com>
2019-09-23 23:56:52 +0300

    fix syntax errors

--- AutoDisposable/Souces/AutoDisposable.swift
+++ AutoDisposable/Souces/AutoDisposable.swift
@@ -11,12 +11,12 @@ import RxSwift
 public protocol AutoDisposable: class {
     
     /// You must dispose these subscribtions from  place where you need. `deinit` method should be a good place.
-    public var subscriptions: [Disposable] { get set }
+    var subscriptions: [Disposable] { get set }
 }
 
 public class AutoDisposableController: UIViewController, AutoDisposable {
     
-    var subscriptions = [Disposable]()
+    public var subscriptions = [Disposable]()
     
     deinit {
         subscriptions.forEach { $0.dispose() }
@@ -25,7 +25,7 @@ public class AutoDisposableController: UIViewController, AutoDisposable {
 
 public class AutoDispose: AutoDisposable {
     
-    var subscriptions = [Disposable]()
+    public var subscriptions = [Disposable]()
     
     deinit {
         subscriptions.forEach { $0.dispose() }
